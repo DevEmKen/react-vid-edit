3aa221ece40861f1405a8e04a881a3d99f6b19be
DevEmKen <emken1997@gmail.com>
2024-03-12 03:23:02 -0700

    got material tailwind working, rough homepage set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import MyNav from "./components/MyNav";
+import "./App.css";
+
+import { Button, Typography, Card } from "@material-tailwind/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDragging, setIsDragging] = useState(false);
+  const [file, setFile] = useState([]);
+
+  const handleDragEnter = (event: any) => {
+    // Need to check if event.dataTransfer has files
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDragOver = (event: any) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: any) => {
+    event.preventDefault();
+    const droppedFiles = event.dataTransfer.files;
+    setFile(droppedFiles); // Update state with dropped files
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="">
+      <MyNav />
+
+      <Typography
+        variant="h1"
+        color="blue-gray"
+        className="text-center font-bold mb-8 pt-20"
+        placeholder={undefined}
+      >
+        Online Editor
+      </Typography>
+      <Typography
+        variant="h3"
+        color="blue-gray"
+        className="text-center font-bold mb-8 py-8"
+        placeholder={undefined}
+      >
+        Upload your videos to our website, edit them, then download them yo
+      </Typography>
+
+      <Card
+        className={`mx-auto max-w-lg flex ${isDragging ? "bg-blue-500" : ""}`}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+        placeholder={undefined}
+      >
+        <Typography color="blue-gray" placeholder={undefined}>
+          <p className="text-center text-gray-700 mb-8">
+            Drag and drop or click to select...
+          </p>
+        </Typography>
+        <div className="flex justify-center content-center">
+          <Button
+            className="max-w-xs mb-4"
+            variant="outlined"
+            color="blue"
+            size="md"
+            placeholder={undefined}
+          >
+            Upload
+          </Button>
+        </div>
+      </Card>
+
+      <div className="mx-auto max-w-screen-md py-12">
+        <Card className="mb-12 overflow-hidden" placeholder={undefined}>
+          <img
+            alt="nature"
+            className="h-[32rem] w-full object-cover object-center"
+            src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
+          />
+        </Card>
+        <Typography
+          variant="h2"
+          color="blue-gray"
+          className="mb-2"
+          placeholder={undefined}
+        >
+          Check out what it do
+        </Typography>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
